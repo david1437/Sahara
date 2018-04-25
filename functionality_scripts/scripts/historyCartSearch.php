e238f9a8a0e6e624e868b16754731229627f93a3
= <=>
2018-04-24 21:29:13 -0400

    even more scripts2

--- functionality_scripts/scripts/historyCartSearch.php
+++ functionality_scripts/scripts/historyCartSearch.php
@@ -4,10 +4,10 @@ $response = array();
 
 // Making sure that the right method is requested.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (isset($_POST['query']) && isset($_POST['u_email']) && isset($_POST['sort'])) {
+  if (isset($_POST['query']) && isset($_POST['u_email']) && isset($_POST['sort']) && isset($_POST['type'])) {
     $db = new DBManipulation();
 
-    $db_response          = $db->searchHistoryCart($_POST['u_email'], $_POST['query'], $_POST['sort']);
+    $db_response          = $db->searchHistoryCart($_POST['u_email'], $_POST['query'], $_POST['sort'], $_POST['type']);
     $response['error']    = false;
     $response['message']  = "Products fetched successfully!";
     $response['products'] = $db_response;
